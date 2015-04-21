c0f2a97938b9e26ce8b10f264bffcced0ad1222e
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-21 09:41:23 -0700

    Adding implementation and full test coverage

--- lib/factor-connector-travis.rb
+++ lib/factor-connector-travis.rb
@@ -4,6 +4,84 @@ require 'travis'
 class TravisConnectorDefinition < Factor::Connector::Definition
   id :travis
 
+  def initialize_travis(params)
+    access_token = params.varify(:access_token, name:'Access Token')
+    github_token = params.varify(:github_token, name:'Github Token')
+    use_pro      = params.varify(:pro, one_of:[true,false], default:false)
+
+    fail 'Must specify :access_token or :github_token' unless access_token || github_token
+    fail 'Must specify :access_token or :github_token, but not both' if access_token && github_token
+
+    travis = use_pro ? Travis::Pro : Travis
+    product_name = travis.class.name == 'Pro' ? 'Travis Pro' : 'Travis'
+
+    begin
+      if access_token
+        info "Connecting to #{product_name} with an access token"
+        travis.access_token = access_token
+        travis::User.current # forces a validation of the access_token
+        info "Connected to #{product_name} with an access token"
+      elsif github_token
+        info "Connecting to #{product_name} with a Github token"
+        travis.github_auth github_token
+        info "Connected to #{product_name} with a Github token"
+      end
+    rescue
+      fail "Authentication failed, check your tokens"
+    end
+
+    travis
+  end
+
+
   action :rebuild do |params|
+    travis       = initialize_travis(params)
+    build_number = params.varify(:build)
+    repo_slug    = params.varify(:repo, required:true)
+    build        = nil
+
+    info "Looking up repo `#{repo_slug}`"
+    begin
+      repo = travis::Repository.find(repo_slug)
+    rescue
+      fail "Failed to find the repo '#{repo_slug}'"
+    end
+
+    begin
+      build = if build_number
+        info "Looking up build #{build_number}"
+        repo.build(build_number)
+      else
+        info 'Looking up last build'
+        build = repo.last_build
+      end
+    rescue
+    end
+
+    fail "No such build was found" unless build
+
+    info "Restarting build ##{build.number}"
+    begin
+      build.restart
+    rescue
+      fail 'Failed to restart the build'
+    end
+
+    build_info = {
+      repository_id:       build.repository_id,
+      commit_id:           build.commit_id,
+      number:              build.number,
+      pull_request:        build.pull_request,
+      pull_request_number: build.pull_request_number,
+      pull_request_title:  build.pull_request_title,
+      config:              build.config,
+      state:               build.state,
+      started_at:          build.started_at,
+      finished_at:         build.finished_at,
+      duration:            build.duration,
+      job_ids:             build.job_ids
+    }
+
+    respond build_info
   end
 end
--- spec/conector_travis_spec.rb
+++ spec/conector_travis_spec.rb
@@ -1,7 +1,76 @@
 require 'spec_helper'
 
 describe TravisConnectorDefinition do
-  it ':: rebuild' do
-    expect(true).to be(true)
+  before do
+    @github_token = ENV['GITHUB_API_KEY']
+    @access_token = ENV['TRAVIS_ACCESS_TOKEN']
+    @runtime = Factor::Connector::Runtime.new(TravisConnectorDefinition)
+  end
+
+  describe :rebuild do
+    it 'fails if github_token and access_token are passed' do
+      @runtime.run([:rebuild], github_token:@github_token, access_token:@access_token)
+      expect(@runtime).to fail 'Must specify :access_token or :github_token, but not both'
+    end
+
+    it 'fails if neither github_token and access_token are passed' do
+      @runtime.run([:rebuild], {})
+      expect(@runtime).to fail 'Must specify :access_token or :github_token'
+    end
+
+    it 'can authenticate with access_token' do
+      @runtime.run([:rebuild], access_token:@access_token)
+      expect(@runtime).to message info:"Connected to Travis with an access token"
+    end
+
+    it 'can authenticate with github_token' do
+      @runtime.run([:rebuild], github_token:@github_token)
+      expect(@runtime).to message info:"Connected to Travis with a Github token"
+    end
+
+    it 'fails if no repo is specified' do
+      @runtime.run([:rebuild], github_token:@github_token)
+      expect(@runtime).to fail 'Repo (:repo) is required'
+    end
+
+    it 'fails if bad repo is specified' do
+      fake_repo = 'skierkowski/this-does-not-exist'
+      @runtime.run([:rebuild], github_token:@github_token, repo:fake_repo)
+      expect(@runtime).to fail "Failed to find the repo '#{fake_repo}'"
+    end
+
+    it 'can rebuild with build_number' do
+      @runtime.run([:rebuild], github_token:@github_token, repo:'skierkowski/hello', build:1)
+      expect(@runtime).to message info:"Looking up build 1"
+      expect(@runtime).to respond
+    end
+
+    it 'fails on bad build number' do
+      @runtime.run([:rebuild], github_token:@github_token, repo:'skierkowski/hello', build:99999)
+      expect(@runtime).to message info:"Looking up build 99999"
+      expect(@runtime).to fail 'No such build was found'
+    end
+
+    it 'can rebuild without build_number' do
+      @runtime.run([:rebuild], github_token:@github_token, repo:'skierkowski/hello')
+      expect(@runtime).to respond
+
+      data = @runtime.logs.last[:data]
+
+      expect(data).to be_a(Hash)
+      expect(data).to include(:repository_id)
+      expect(data).to include(:commit_id)
+      expect(data).to include(:number)
+      expect(data).to include(:pull_request)
+      expect(data).to include(:pull_request_numbe)
+      expect(data).to include(:pull_request_titl)
+      expect(data).to include(:config)
+      expect(data).to include(:state)
+      expect(data).to include(:started_at)
+      expect(data).to include(:finished_at)
+      expect(data).to include(:duration)
+      expect(data).to include(:job_ids)
+    end
+
   end
 end
